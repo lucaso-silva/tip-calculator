04a6be9e2f763039dc9c8d62ef0132c196410d0c
lucaso-silva <luc.osilva@gmail.com>
2023-11-05 19:29:03 -0800

    feat: displayTipAndTotal

--- script.js
+++ script.js
@@ -3,24 +3,64 @@
 const inputBill = document.getElementById("bill");
 const inputNumPeople = document.getElementById("numPeople");
 const percentValues = document.querySelectorAll(".pValue");
+const tipValue = document. querySelector(".tip-value");
+const totalValue = document.querySelector(".total-value");
+
+let bill;
+let numPeople;
 
 
 inputBill.addEventListener("blur", ()=> {
-    let bill = inputBill.value;
-    console.log(bill);
+    bill = +inputBill.value;
 });
 
 inputNumPeople.addEventListener("blur", ()=> {
-    let numPeople = inputNumPeople.value;
+    numPeople = +inputNumPeople.value;
     console.log(numPeople);
 });''
 
 for(let i = 0; i < percentValues.length; i++) {
     percentValues[i].addEventListener("input", ()=>{
-        calculateTip(percentValues[i].id);
+        displayTipAndTotal(percentValues[i].id);
     })
 }
 
-function calculateTip(percId) {
-    console.log(percId);
+function displayTipAndTotal(percId) {
+
+    let tipAmount = "";
+    let total;
+
+    switch(percId) {
+        case "pc5":
+            total = calculateTip(5);
+            break;
+        case "pc10":
+            total = calculateTip(10);
+            break;
+        case "pc15":
+            total = calculateTip(15);
+            break;
+        case "pc25":
+            total = calculateTip(25);
+            break;
+        case "pc50":
+            total = calculateTip(50);
+            break;
+        default:
+            total = 100;
+            break;
+    }
+
+    tipAmount = total - bill;
+    console.log("bill + tips: " + total);
+    console.log("tip: " + (total - bill));
+
+    tipValue.innerHTML = `$ ${tipAmount/numPeople}`;
+    totalValue.innerHTML = `$ ${total/numPeople}`
+}
+
+function calculateTip(percentage) {
+    return bill + (bill * (percentage/100));
+
+    console.log("total bill: " + total);
 }
